c0973d63f56f2f3059cfe8b353af9423a24182b6
cenry <cenruyi123@gmail.com>
2020-04-01 23:20:30 +0800

    annotation

--- src/com/andycen/spring/annotation/AutowiredTest.java
+++ src/com/andycen/spring/annotation/AutowiredTest.java
@@ -5,16 +5,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AutowiredTest {
 
-    private AnnotationInterface annotationInterface;
+//    private AnnotationInterface annotationInterface;
+//
+//    @Autowired
+//    public void setAnnotationInterface(AnnotationInterface annotationInterface) {
+//        this.annotationInterface = annotationInterface;
+//    }
 
-    @Autowired
-    public void setAnnotationInterface(AnnotationInterface annotationInterface) {
-        this.annotationInterface = annotationInterface;
+//    private AnnotationInterface[] annotationInterfaces;
+//    public AutowiredTest(AnnotationInterface[] annotationInterfaces) {
+//        this.annotationInterfaces = annotationInterfaces;
+//    }
+
+    private AnnotationClassA annotationClassA;
+
+    public AutowiredTest(AnnotationClassA annotationClassA) {
+        this.annotationClassA = annotationClassA;
     }
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("annotation.xml");
         AutowiredTest autowiredTest = applicationContext.getBean(AutowiredTest.class);
-        System.out.println(autowiredTest.annotationInterface.getClass().getName());
+//        System.out.println(autowiredTest.annotationInterface.getClass().getName());
+//        System.out.println(autowiredTest.annotationInterfaces.length);
+        System.out.println(autowiredTest.annotationClassA.getClass().getName());
     }
 }
